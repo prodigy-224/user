dd76d790bc6e5bec9c95a31d4f6735179041cdf0
prodigy-224 <wanjirum.leah@gmail.com>
2024-03-07 13:53:57 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name : ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+
+print(f"Hey {name}, your age is {age}, and your location is {location}.")
